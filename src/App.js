ab2fef23b47bd355c712ace066e4ef44c40d98fc
Shivam <shivamkasoudhan7@gmail.com>
2023-03-31 02:54:16 +0530

    contain some error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-
+import Banner from './Components/Banner';
 function App() {
   return (
      <>
         <Navbar/>
+        <Banner/>
      </>
   );
 }
