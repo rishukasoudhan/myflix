8c88bc3d77fa8c914f8398269f02abf5d831fdc3
Shivam <shivamkasoudhan7@gmail.com>
2023-03-31 00:37:47 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <h1>hello shivam</h1>
+     <>
+        <Navbar/>
+     </>
   );
 }
 
